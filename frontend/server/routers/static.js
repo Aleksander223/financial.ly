ac1734737deaf7bd138e42ea1abb2a8a601f2983
iustin <iustin@privacysystems.eu>
2020-03-31 12:34:41 +0300

    Add fixes

--- frontend/server/routers/static.js
+++ frontend/server/routers/static.js
@@ -2,6 +2,24 @@ const path = require("path");
 const express = require("express");
 
 
-const staticRouter = express.static(path.join(__dirname, "..", "..", "./static"))
+const staticRouter = express.Router();
+
+staticRouter.use(express.static('../static')); // <- ia doar fisierele din static
+
+staticRouter.get("/login", (req, res) => {
+  res.sendFile("login.html", {
+    root: path.join(__dirname, "..", "..", "/static")
+  });
+});
+
+staticRouter.get("/register", (req, res) => {
+  res.sendFile("register.html", {
+    root: path.join(__dirname, "..", "..", "./static")
+  });
+});
+
+staticRouter.get("*", function(req, res){
+  res.send('NOT FOUND', 404);
+});
 
 module.exports = staticRouter;
