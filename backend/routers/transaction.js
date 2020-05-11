58030421c1f1fd8ff6eb3f58ae807640831d3c99
Alexandru-Vlad <alexandruvlad.adam@gmail.com>
2020-05-12 01:20:17 +0300

    Added transactions to dash

--- backend/routers/transaction.js
+++ backend/routers/transaction.js
@@ -2,15 +2,24 @@ const express = require("express");
 const auth = require("../middleware/auth");
 
 const Transaction = require("../models/transaction.js");
-const transactionController = require("../services/tranzactionController");
-
-
+const transactionController = require("../services/transactionController");
 
 const transactionRouter = express.Router();
 
-transactionRouter.get("/transactions", auth, transactionController.getTransactions);
-transactionRouter.post("/createTransaction", auth, transactionController.addTransactions);
-transactionRouter.post("/cancelTransaction", auth, transactionController.cancelTransactions);
-
+transactionRouter.get(
+  "/transactions",
+  auth,
+  transactionController.getTransactions
+);
+transactionRouter.post(
+  "/createTransaction",
+  auth,
+  transactionController.addTransactions
+);
+transactionRouter.post(
+  "/cancelTransaction",
+  auth,
+  transactionController.cancelTransactions
+);
 
 module.exports = transactionRouter;
--- frontend/static/res/scripts/dash.js
+++ frontend/static/res/scripts/dash.js
@@ -10,6 +10,74 @@ $(document).ready(async () => {
   const userData = await userD.json();
 
   $("#username").text(userData.username);
+
+  table = $("#example").DataTable({
+    autoWidth: false,
+    lengthChange: false,
+    paging: false,
+    ordering: false,
+    info: false,
+    responsive: true,
+    searching: false,
+    ajax: {
+      url: "http://localhost:9999/res/mock/ron.json",
+    },
+    columns: [
+      //   {
+      //     data: "timestamp",
+      //     title: "Date",
+      //     type: "date",
+      //     render(val) {
+      //       const date = new Date(val);
+      //       return moment(date).format("HH:mm DD MMM");
+      //     },
+      //   },
+      //   { data: "sender", title: "Sender" },
+      //   { data: "receiver", title: "Receiver" },
+      //   {
+      //     data: "sum",
+      //     title: "Sum",
+      //     render(val, type, full, meta) {
+      //       return val + " " + full.wallet;
+      //     },
+      //   },
+      {
+        data: "sum",
+        title: "",
+        width: "100%",
+        render(val, type, full, meta) {
+          //   return val + " " + full.wallet;
+          let sign;
+          let verb;
+
+          const date = new Date(val);
+
+          if (full.sum < 0) {
+            sign = "-";
+            verb = "To";
+          } else {
+            sign = "+";
+            verb = "From";
+          }
+
+          //   return `<h5>${verb} ${full.sender}</h5><p>${full.sum}</p>`;
+          return `
+          <div class="uk-card uk-card-body uk-card-default uk-grid">
+          <div class="uk-width-1-2"><h5 class="uk-text-secondary">${verb} ${
+            full.sender
+          }</h3></div>
+          <div class="uk-width-1-2"><h5 class="uk-text-secondary">${sign} ${Math.abs(
+            full.sum
+          )} ${full.wallet}</h3></div>
+          <div><h3 class="uk-text-meta">${moment(date).format(
+            "HH:mm DD MMM"
+          )}</h3></div>
+          </div>
+          `;
+        },
+      },
+    ],
+  });
 });
 
 function signOut() {
