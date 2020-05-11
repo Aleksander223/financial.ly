4e4cc22a241f053ab579182456e898e9a2d10587
Alexandru-Vlad <alexandruvlad.adam@gmail.com>
2020-05-11 23:25:04 +0300

    Added wallet

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,83 +3,90 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
-
 const userSchema = mongoose.Schema({
-    username: {
-       type: String,
-       required: true,
-       unique: true,
-       trim: true,
-    },
-    email: {
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7,
+  },
+  wallet: [
+    {
+      currency: {
         type: String,
         required: true,
-        unique: true,
-        trim: true,
+      },
+      amount: {
+        type: Number,
+        default: 0,
+      },
     },
-    password: {
-        type: String,
-        required: true,
-        minLength: 7,
-    },
-    // firstName: {
-    //     type: String,
-    //     required: true
-    // },
-    // lastName: {
-    //     type: String,
-    //     required: true
-    // },
-    // phoneNumber: {
-    //     type: String,
-    //     required: true
-    // },
-    // address: {
-    //     type: String,
-    //     required: true
-    // },
-    // country: {
-    //     type: String,
-    //     required: true
-    // },
-    // birthDate: {
-    //     type: String,
-    //     required: true
-    // }
+  ],
+  // firstName: {
+  //     type: String,
+  //     required: true
+  // },
+  // lastName: {
+  //     type: String,
+  //     required: true
+  // },
+  // phoneNumber: {
+  //     type: String,
+  //     required: true
+  // },
+  // address: {
+  //     type: String,
+  //     required: true
+  // },
+  // country: {
+  //     type: String,
+  //     required: true
+  // },
+  // birthDate: {
+  //     type: String,
+  //     required: true
+  // }
 });
 
 userSchema.pre("save", async function (next) {
-    const user = this;
+  const user = this;
 
-    if (user.isModified("password") || user.isNew("password")) {
+  if (user.isModified("password") || user.isNew("password")) {
     user.password = await bcrypt.hash(user.password, process.env.SALT_ROUNDS);
-    }
+  }
 
-    next();
+  next();
 });
 
 userSchema.methods.generateAuthToken = async function () {
-    const user = this;
-    const token = jwt.sign(
-        { _id: user._id }, 
-        process.env.JWT_KEY, 
-        { expiresIn: "30m"}
-    );
+  const user = this;
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY, {
+    expiresIn: "30m",
+  });
 
-    return token;
+  return token;
 };
 
 userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await UserModel.findOne({ email });
+  const user = await UserModel.findOne({ email });
 
-    if (!user || !(await bcrypt.compare(password, user.password)))
+  if (!user || !(await bcrypt.compare(password, user.password)))
     throw new Error({ error: "Invalid login credentials" });
 
-    return user;
+  return user;
 };
 
 const UserModel = mongoose.model("user", userSchema);
-
 
 module.exports = UserModel;
---
+++ backend/services/walletController.js
@@ -0,0 +1,9 @@
+const User = require("../models/user.js");
+
+const getCurrencies = (req, res) => {
+  user = req.user;
+};
+
+module.exports = {
+  getCurrencies,
+};
